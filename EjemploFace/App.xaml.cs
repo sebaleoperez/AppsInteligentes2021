da8c46cfecf833b0e33ab93c3531b68e998216d4
Seba Leo Perez <39063689+sebaleoperez@users.noreply.github.com>
2021-09-09 23:20:14 -0300

    Add files via upload

---
+++ EjemploFace/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EjemploFace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new VerificacionPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ EjemploFace/VerificacionPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using Xamarin.Forms;
+
+namespace EjemploFace
+{
+    public partial class VerificacionPage : ContentPage
+    {
+        public VerificacionPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            imagen1.Source = entryImagen1.Text;
+            imagen2.Source = entryImagen2.Text;
+
+            // Creo el cliente
+            FaceClient faceClient = new FaceClient(new ApiKeyServiceClientCredentials(AppSettings.FaceAPIKey));
+            faceClient.Endpoint = AppSettings.FaceAPIEndPoint;
+
+            IList<FaceAttributeType> atributos = new List<FaceAttributeType>();
+            atributos.Add(FaceAttributeType.Age);
+            atributos.Add(FaceAttributeType.Emotion);
+            atributos.Add(FaceAttributeType.Hair);
+            atributos.Add(FaceAttributeType.Glasses);
+
+            // Detecto las caras
+            var resultadoImagen1 = await faceClient.Face.DetectWithUrlAsync(entryImagen1.Text, true, true, atributos);
+
+            var resultadoImagen2 = await faceClient.Face.DetectWithUrlAsync(entryImagen2.Text, true, true, atributos);
+
+            // Verifico las caras
+            var verificationResult = await faceClient.Face.VerifyFaceToFaceAsync(
+                resultadoImagen1[0].FaceId.Value, resultadoImagen2[0].FaceId.Value);
+
+            labelResultado.Text = verificationResult.Confidence.ToString();
+        }
+    }
+}
